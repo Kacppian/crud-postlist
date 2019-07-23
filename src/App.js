9bb62634678f38158411aa124082c1a351f9f1b5
Kaushik ASP <aspkaushik@gmail.com>
2019-07-23 07:07:07 +0530

    Fixes Post import

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import { makeStyles } from "@material-ui/core/styles";
 import Header from "./components/Header";
 import Container from "@material-ui/core/Container";
-import Posts from "./components/Posts";
+import Posts from "./containers/Posts";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
 const useStyles = makeStyles(theme => ({
